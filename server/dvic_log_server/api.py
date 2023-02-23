ff976634b0e607f71e1f126be00dc98b7a6a0dc9
sn00wden <matthieu2603@gmail.com>
2023-02-23 10:19:33 +0100

    Close error

--- server/dvic_log_server/api.py
+++ server/dvic_log_server/api.py
@@ -27,11 +27,16 @@ async def websocket_endpoint(websocket: WebSocket):
     print(f'New connection from {websocket.client.host}:{websocket.client.port}')
     try:
         # Log the connection into the log file
-        with open(os.path.join(os.path.dirname(__file__), '.logs/websockets_connection.log'), 'a') as log_file:
+        path = os.path.dirname(os.path.realpath(__file__))
+        #remove the 'dvic_log_server' part of the path
+        path = path[:path.rfind('/')]
+        with open(os.path.join(path, '.logs/websockets_connection.log'), 'a') as log_file:
             log_file.write(f'New connection from {websocket.client.host}:{websocket.client.port}\n')
+            print("Logged connection to file")
         while True:
             data = await websocket.receive_text()
             await websocket.send_text(f"Message text was: {data}")
+            
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         await websocket.close()
