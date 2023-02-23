c01ed3609230f56dce0d9ed8d08640d7f5319d4e
sn00wden <matthieu2603@gmail.com>
2023-02-23 15:22:06 +0100

    Adding client services

---
+++ node/install_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Create a directory for the installation
+mkdir -p /opt/dvic_demo_watcher/
+
+# Fetch the file from github
+curl -O https://raw.githubusercontent.com/matthieu-sgi/DVIC_Demo_Monitoring/main/node/client/dvic_client.py
+
+# Move the file to the installation directory
+mv dvic_client.py /opt/dvic_demo_watcher/
+
+# Create a service file
+cat <<EOF > /etc/systemd/system/dvic_demo_watcher.service
