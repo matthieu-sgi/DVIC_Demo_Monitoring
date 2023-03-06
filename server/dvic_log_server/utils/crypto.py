350e55cea6cbddf746bd3c35baa7054affa83649
Grégor JOUET <gregor.jouet@laposte.net>
2023-03-07 00:45:13 +0100

    fix crypto

--- server/dvic_log_server/utils/crypto.py
+++ server/dvic_log_server/utils/crypto.py
@@ -6,11 +6,15 @@ import base64
 import os
 
 from ecdsa import SigningKey, VerifyingKey
+import ecdsa
 # private_key = SigningKey.generate() # uses NIST192p
 # signature = private_key.sign(b"Educative authorizes this shot")
 # print(signature)
 # public_key = private_key.verifying_key
 # print("Verified:", public_key.verify(signature, b"Educative authorizes this shot"))
+
+UUID_LEN = 36
+SALT_LEN = 16
 
 class CryptClient():
     def __init__(self, public_key: Union[Path, bytes] = None, private_key: Union[Path, str] = None) -> None:
@@ -33,9 +37,10 @@ class CryptClient():
         if type(k) == Path:
             if k.exists() and k.is_file():
                 with open(k, 'r') as fh: return fh.read()
+        raise Exception("Cannot load private key")
 
     def get_salt(self) -> str:
-        return ''.join(random.choices(string.ascii_lowercase + string.digits, k = 16))
+        return ''.join(random.choices(string.ascii_lowercase + string.digits, k = SALT_LEN))
     
     def sign(self, msg: str) -> str:
         return base64.b64encode(self.private_key.sign(msg.encode())).decode()
@@ -47,18 +52,20 @@ class CryptClient():
     #     return self.private_key.to_cryptography_key().decrypt(base64.b64decode(msg), padding.PKCS1v15()).decode()
 
     def verify(self, key: VerifyingKey, plaintext: str, signature: str) -> bool:
-        return key.verify(base64.b64decode(signature), plaintext.encode())
+        try: return key.verify(base64.b64decode(signature), plaintext.encode())
+        except ecdsa.keys.BadSignatureError: return False
 
     def craft_initial_token(self, uid: str):
         salt = self.get_salt()
-        plaintext = f'{uid}|{salt}'
-        signature = self.sign(plaintext)
-        return f'{plaintext}.{signature}'
+        plaintext = f'{uid}{salt}'
+        signature = client.encode_b64_for_url(self.sign(plaintext))
+        return f'{plaintext}{signature}'
     
     def verify_initial_packet(self, pck: str, pub_key: str) -> bool:
         key = VerifyingKey.from_pem(self._read_key(pub_key))
-        plaintext, signature = pck.split('.')
-        return self.verify(key, plaintext, signature)
+        CUTOFF = SALT_LEN + UUID_LEN
+        plaintext, signature = pck[:CUTOFF], pck[CUTOFF:]
+        return self.verify(key, plaintext, self.decode_b64_from_url(signature))
     
     def encode_b64_for_url(self, b64: str):
         # '+/=' > '*_-'
@@ -71,8 +78,8 @@ if __name__ == '__main__':
     import uuid
     client = CryptClient(private_key='./testing/client1.private')
     u = 'e118857e-3732-4e58-aa9c-56685c6a6492' # str(uuid.uuid4())
-    pck = client.encode_b64_for_url(client.craft_initial_token(u))
+    pck = client.craft_initial_token(u)
     print(pck)
-
     server = CryptClient(private_key='./testing/api.private')
-    print(server.verify_initial_packet(server.decode_b64_from_url(pck), './testing/client1.public'))
+
+    print(server.verify_initial_packet(pck, './testing/client1.public'))
